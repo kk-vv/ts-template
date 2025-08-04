fbb18c4c920934d687af59b08da15af60d03a7f5
Felix.Hu <hulj1204@yahoo.com>
2025-08-04 16:29:29 +0800

    [U] Update version

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra')
 const prompts = require('prompts');
 
 program
-  .version('1.1.0')
+  .version('1.1.1')
   .command('create')
   .description('Create a new project from template')
   .action(async () => {
