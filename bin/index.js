81c7b8bac8f34f870f53a3c8b764daa63d471b18
Felix.Hu <hulj1204@yahoo.com>
2025-08-12 15:14:15 +0800

    [U] Update command name to ts-gc-express

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra')
 const prompts = require('prompts');
 
 program
-  .version('1.3.1')
+  .version('1.4.0')
   .command('create')
   .description('Create a new project from template')
   .action(async () => {
